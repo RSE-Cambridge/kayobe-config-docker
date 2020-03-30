cec4bd846c2f3f7ca2d0017adac918ae119b5724
Will Szumski <will@stackhpc.com>
2020-03-30 15:27:39 +0000

    Fix htpasswd
    The password is only printed when a new one is generatred.

--- jenkins-setup.sh
+++ jenkins-setup.sh
@@ -11,17 +11,17 @@ sudo docker push 10.200.0.1:4000/jenkins-docker:latest
 sudo mkdir -p /etc/nginx-proxy/htpasswd
 sudo mkdir -p /etc/nginx-proxy/certs
 
+export jenkins_virtual_host="10.60.150.1"
+
 export admin_basic_passwd=$(openssl rand -base64 32)
 export admin_basic_htpasswd=$(printf "admin:$(openssl passwd -apr1 $admin_basic_passwd )\n")
-export nginx_htpasswd_file="/etc/nginx-proxy/htpasswd/jenkins.htpasswd"
-sudo -E sh -c 'test -f "${nginx_htpasswd_file}" || echo "${admin_basic_htpasswd}" > "${nginx_htpasswd_file}"'
+export nginx_htpasswd_file="/etc/nginx-proxy/htpasswd/${jenkins_virtual_host}"
+export password_hint="Jenkins basic auth admin password:
+${admin_basic_passwd}"
+sudo -E sh -c 'test -f "${nginx_htpasswd_file}" || (echo "${admin_basic_htpasswd}" > "${nginx_htpasswd_file}" && echo "$password_hint")'
 
-# Ensure non-world readable
-sudo -E sh -c 'chown root:root "${nginx_htpasswd_file}" && chmod 660 "${nginx_htpasswd_file}"'
-
-echo "Jenkins basic auth admin password:"
-echo "$(sudo -E cat $nginx_htpasswd_file)"
-echo ""
+# Ensure non-world readable - 101 is the uid of the nginx user in the container
+sudo -E sh -c 'chown 101:101 "${nginx_htpasswd_file}" && chmod 660 "${nginx_htpasswd_file}"'
 
 echo "nginx_proxy container id:"
 sudo docker run --name=nginx_proxy \
@@ -41,7 +41,7 @@ sudo cp -r jenkins-config/ /var/jenkins-home/
 echo "jenkins container id:"
 sudo docker run --name=jenkins \
      --env="VIRTUAL_PORT=8080" \
-     --env="VIRTUAL_HOST=10.200.0.1" \
+     --env="VIRTUAL_HOST=${jenkins_virtual_host}" \
      --volume="/var/jenkins-home:/var/jenkins-home:rw" \
      --volume="/var/run/docker.sock:/var/run/docker.sock:ro" \
      -p 0.0.0.0:18080:8080 \
